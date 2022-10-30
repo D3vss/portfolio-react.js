ccf49a417a66e2ca4eeb0ed6c4e92f3b09b551f0
d3vss <mohamed_oudoun@um5.ac.ma>
2022-10-30 23:10:52 +0100

    Home page layout done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ function Layout({ children }) {
   return (
     <div className="bg-black">
       <Navbar />
+      <div className="container mx-auto h-1 bg-blue-700"></div>
       <div className="container mx-auto text-white">{children}</div>
     </div>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,14 @@ import React from "react";
 import { Layout } from "../../components";
 
 function HomePage(props) {
-  return <Layout />;
+  return (
+    <Layout>
+      <div className="flex flex-row justify-evenly">
+        <div className="bg-red-500 w-full h-screen">sec1</div>
+        <div className="bg-blue-500 w-full h-screen">sec2</div>
+      </div>
+    </Layout>
+  );
 }
 
 export default HomePage;
