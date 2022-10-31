6133741b840832ff2d21ae998b02803f4f3c597f
d3vss <mohamed_oudoun@um5.ac.ma>
2022-10-31 12:56:16 +0100

    section1 improved + footer added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-import { Layout } from "../../components";
+import { Layout, Button } from "../../components";
 
 function HomePage(props) {
   return (
     <Layout>
       <div className="flex flex-row justify-evenly h-full">
-        <div className="bg-red-500 w-full ">sec1</div>
-        <div className="bg-blue-500 w-full ">sec2</div>
+        <section className="w-full  flex flex-col">
+          <p className="text-5xl font-bold mb-5">
+            Hi! <br /> I am Oudoun Mohamed.
+          </p>
+          <p className="text-base font-normal mb-10">
+            A Web and Mobile Developer, and a Data enthusiast.
+          </p>
+
+          <div className="flex flex-row w-4/12 justify-between">
+            <Button label={"Hire Me!"} variant="filled" />
+            <Button label={"Projects"} variant="outlined" />
+          </div>
+          <div className="container px-20 mx-auto h-1 bg-blue-700" />
+          <footer className="mt-auto container bg-gray-700 h-1/2"></footer>
+        </section>
+        <section className="grayscale bg-me bg-center bg-contain bg-no-repeat w-full "></section>
       </div>
     </Layout>
   );
